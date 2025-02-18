1244c5c65412333075ecf650d6db0792de397f22
cfprabhu <cfprabhu@yahoo.com>
2025-02-18 05:33:19 +0530

    first commit

---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,23 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private double price;
+}
